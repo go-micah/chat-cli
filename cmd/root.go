e25cf537d253778dea2e456daee0bb609d311e14
Micah Walter <47419+micahwalter@users.noreply.github.com>
2024-02-15 18:54:20 +0000

    more help

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "chat-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Chat with LLMs from Amazon Bedrock!",
+	Long:  `This is a command line tool that allows you to chat with LLMs from Amazon Bedrock!`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
